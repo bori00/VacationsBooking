25c72634aea44adee3d64a89dacdffada86ffbef
bori00 <fazakasbori@gmail.com>
2022-03-04 23:56:16 +0200

    define VacationPackage class - no relationships added

---
+++ src/main/java/model/VacationPackage.java
@@ -0,0 +1,44 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "vacation_package")
+@NoArgsConstructor
+@Getter
+public class VacationPackage {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private float price;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Column
+    private String extraDetails;
+
+    @Column(nullable = false)
+    private Integer nrPlaces;
+
+    public VacationPackage(String name, float price, LocalDate startDate, LocalDate endDate, String extraDetails, Integer nrPlaces) {
+        this.name = name;
+        this.price = price;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.extraDetails = extraDetails;
+        this.nrPlaces = nrPlaces;
+    }
+}
