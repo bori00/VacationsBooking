dceb5dd4bdb1d720c9197584f3981969e8e99973
bori00 <fazakasbori@gmail.com>
2022-03-05 21:52:27 +0200

    implement DestinationService

--- src/main/java/service/view_models/DestinationViewModel.java
+++ src/main/java/service/view_models/DestinationViewModel.java
@@ -2,10 +2,15 @@ package service.view_models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import model.Destination;
 
 @AllArgsConstructor
 @Getter
 public class DestinationViewModel {
     private final Long id;
     private final String name;
+
+    public DestinationViewModel(Destination destination) {
+        this(destination.getId(), destination.getName());
+    }
 }
