ca64e09f1f66bbd1248f925e2e2796eaf3069669
bori00 <fazakasbori@gmail.com>
2022-03-07 12:39:23 +0200

    add JavaFX Application class

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,40 @@
+import controller.loginregisterview.LoginPaneController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Starter class of the JavaFX application.
+ */
+public class MainApp extends Application {
+    private AnchorPane root;
+    private Scene scene;
+
+    /** Method called when the application is started. */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        loadRoot();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Vacation Booking");
+        primaryStage.show();
+    }
+
+    private void  loadRoot() throws IOException {
+        FXMLLoader mainStageLoader = new FXMLLoader(getClass().getResource("/login_pane.fxml"));
+        root = mainStageLoader.load();
+        LoginPaneController controller = mainStageLoader.getController();
+        // controller.initDeliveryService(deliveryService);
+        root.setPadding(new Insets(10, 10, 10, 10));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
