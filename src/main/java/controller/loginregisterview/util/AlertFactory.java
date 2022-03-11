6e0bf5755aec7d16774a4ca623123cbb12ac0317
bori00 <fazakasbori@gmail.com>
2022-03-11 15:52:08 +0200

    implement AlertFactory

---
+++ src/main/java/controller/loginregisterview/util/AlertFactory.java
@@ -0,0 +1,30 @@
+package controller.loginregisterview.util;
+
+import javafx.scene.control.Alert;
+import service.OperationStatus;
+
+
+public class AlertFactory {
+
+    public static void showAlert(OperationStatus operationStatus) {
+        if (!operationStatus.isSuccessful()) {
+            showErrorAlert(operationStatus);
+        } else {
+            showSuccessAlert(operationStatus);
+        }
+    }
+
+    private static void showErrorAlert(OperationStatus operationStatus) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Unsuccessful Operation");
+        alert.setHeaderText(operationStatus.getErrorMsg());
+        alert.show();
+    }
+
+    private static void showSuccessAlert(OperationStatus operationStatus) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Success");
+        alert.setHeaderText("Successful operation");
+        alert.show();
+    }
+}
